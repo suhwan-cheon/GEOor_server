caa7fcbd0f5783ab6576ab179c908c4346b1c79a
Brorica <minjea917@gmail.com>
2021-10-21 17:49:59 +0900

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+프로젝트 루트 디렉토리에 shp 디렉토리 생성
+.dbf, .prj, .shp, .shx 파일 모두 넣어야
+geotools가 shp 파일 읽기 가능
---
+++ src/main/java/main.java
@@ -0,0 +1,37 @@
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class main {
+    public static void main(String[] args) throws Exception {
+        File file = new File("./shp/NF_A_A01000_A_A01000_000000.shp");
+        Map<String, Object> map = new HashMap<>();
+        map.put("url", file.toURI().toURL());
+
+        DataStore dataStore = DataStoreFinder.getDataStore(map);
+        String typeName = dataStore.getTypeNames()[0];
+
+        FeatureSource<SimpleFeatureType, SimpleFeature> source =
+                dataStore.getFeatureSource(typeName);
+        Filter filter = Filter.INCLUDE; // ECQL.toFilter("BBOX(THE_GEOM, 10,20,30,40)")
+
+        FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures(filter);
+        try (FeatureIterator<SimpleFeature> features = collection.features()) {
+            while (features.hasNext()) {
+                SimpleFeature feature = features.next();
+                System.out.print(feature.getID());
+                System.out.print(": ");
+                System.out.println(feature.getDefaultGeometryProperty().getValue());
+            }
+        }
+    }
+}
