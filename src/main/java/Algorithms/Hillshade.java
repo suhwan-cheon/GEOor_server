5b5238c8f754789019fb0b3f70c1f0499b617d71
suhwan <52690419+suhwan-cheon@users.noreply.github.com>
2021-11-11 13:19:44 +0900

    refactoring hillshade algorithm (#10)

--- src/main/java/Algorithms/Hillshade.java
+++ src/main/java/Algorithms/Hillshade.java
@@ -1,42 +1,41 @@
 package Algorithms;
 
-import sun.SunInfo;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-
-/*
- * convert (sunInfo + dem) -> hillshade
- * */
 public class Hillshade {
-    public ArrayList<ArrayList<Double>> hsConverter(ArrayList<ArrayList<SunInfo>> si, ArrayList<ArrayList<Double>> zValues){
-        ArrayList<ArrayList<Double>> hs = new ArrayList<ArrayList<Double>>();
-        for(int i=1; i<si.size() - 1; i++){
-            for(int j=1; j<si.get(i).size() - 1; j++){
 
+    //hillshade 값
+    private Double hillshade;
 
-                /*
-                3x3 window
-                (a, b, c)
-                (d, e, f) ---> e = which the aspect is being calculated
-                (g, h, i)
-                 */
-                HashMap<Character, Double>windows = new HashMap<Character, Double>();
-                int cnt = 0;
-                for(int y=i-1; y<=i+1; y++){
-                    for(int x=j-1; x<=j+1; x++){
-                        windows.put((char) ('a' + cnt), zValues.get(y).get(x));
-                        cnt++;
-                    }
-                }
+    //x,y 좌표 값
+    private int x;
+    private int y;
 
-                Double Zenith_deg = 90 - si.get(i).get(j).getAltitude_09();
-                Double Zenith_rad = (Zenith_deg * Math.PI) / 180.0;
+    public Hillshade(Double hillshade, int x, int y) {
+        this.hillshade = hillshade;
+        this.x = x;
+        this.y = y;
+    }
 
-                Double Aznimuth_math = 360.0 - si.get(i).get(j).getAzimuth_09();
+    public Double getHillshade() {
+        return hillshade;
+    }
 
-            }
-        }
-        return hs;
+    public void setHillshade(Double hillshade) {
+        this.hillshade = hillshade;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 }
